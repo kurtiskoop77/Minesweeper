18e1798ea23f48741fa4a965d975603c507c7edb
Kurtis Koopmann <KHKoopmann@student.fullsail.edu>
2022-06-08 17:34:50 -0400

    finish

--- cMain.cpp
+++ cMain.cpp
@@ -11,6 +11,7 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MINESWEEPER", wxPoint(400, 100), wx
 
 	mineField = new int[gridWidth * gridHeight];
 
+	wxFont font(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
 	for (int x = 0; x < gridWidth; x++) {
 		for (int y = 0; y < gridHeight; y++) {
 			button[y * gridWidth + x] = new wxButton(this, 10000 + (y * gridWidth + x));
@@ -38,8 +39,43 @@ void cMain::ButtonClicked(wxCommandEvent& event) {
 			int randx = rand() % gridWidth;
 			int randy = rand() % gridHeight;
 			if (mineField[randy * gridWidth + randx] == 0 && randx != x && randy != y) {
-
+				mineField[randy * gridWidth + randy] = -1;
+				mines--;
 			}
+		}
+		firstClick = false;
+	}
+	//Disable Button
+	button[y * gridWidth + x]->Enable(false);
+	if (mineField[y * gridWidth + x] == -1) {
+		wxMessageBox("KABOOM!!!! - GAME OVER");
+
+		//reset game
+		firstClick = true;
+		for (int x = 0; x < gridWidth; x++) {
+			for (int y = 0; y < gridWidth; y++) {
+				mineField[y * gridWidth + x] = 0;
+				button[y * gridWidth + x]->SetLabel("");
+				button[y * gridWidth + x]->Enable(true);
+			}
+		}
+	}
+	else {
+		//count neighboring mines
+		int mCount = 0;
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if (x + i >= 0 && x + i < gridWidth && y + j >= 0 && y + j < gridHeight) {
+					if (mineField[(y + j) * gridWidth + (x + i)] == -1) {
+						mCount++;
+					}
+				}
+			}
+		}
+
+		//update button label to show mine count if > 0
+		if (mCount > 0) {
+			button[y * gridWidth + x]->SetLabel(std::to_string(mCount));
 		}
 	}
 	event.Skip();
